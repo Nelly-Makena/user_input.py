b3f12e46dd394a247baf1e31d8896c3c019dd652
Nelly Makena <nellysolomonmakena3@gmail.com>
2024-03-01 13:53:01 +0300

    user input

---
+++ user_input.py
@@ -0,0 +1,32 @@
+#User's name
+name = input ("Enter your name: ")
+#User's age
+age =  input ("Enter your age:  ")
+#User's location
+location = input ("Enter your location: ")
+#personalized message
+print("Hello {} ,you are {} years old , and  lives in {} ." .format (name ,age , location  ) )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
